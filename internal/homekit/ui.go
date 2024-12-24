21e44a2c73103252acbfc516209080932d48c243
Linghua Zhang <linghua.zhang@me.com>
2024-12-25 01:40:46 +0900

    refactor: styling

--- internal/homekit/ui.go
+++ internal/homekit/ui.go
@@ -2,10 +2,16 @@ package homekit
 
 import (
 	"net/http"
+	"strings"
 )
 
 func (m *HMManager) startUIHandler() {
-	m.server.ServeMux().HandleFunc("/ui", func(res http.ResponseWriter, req *http.Request) {
+	m.server.ServeMux().HandleFunc("/views/components/{name}", func(res http.ResponseWriter, req *http.Request) {
+		path := strings.TrimPrefix(req.URL.Path, "/views/components/")
+		name := strings.SplitN(path, "/", 2)[0]
+		http.ServeFile(res, req, "views/components/"+name)
+	})
+	m.server.ServeMux().HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
 		http.ServeFile(res, req, "views/layouts/main.html")
 	})
 }
