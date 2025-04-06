586ce4b87d83b5b1299d7330818e46acfab48449
Linghua Zhang <linghua.zhang@me.com>
2025-04-06 22:28:44 +0900

    feat: PWA

--- internal/homekit/ui.go
+++ internal/homekit/ui.go
@@ -11,6 +11,9 @@ func (m *HMManager) startUIHandler() {
 		name := strings.SplitN(path, "/", 2)[0]
 		http.ServeFile(res, req, "views/components/"+name)
 	})
+	m.server.ServeMux().HandleFunc("/manifest.json", func(res http.ResponseWriter, req *http.Request) {
+		http.ServeFile(res, req, "views/layouts/manifest.json")
+	})
 	m.server.ServeMux().HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
 		http.ServeFile(res, req, "views/layouts/main.html")
 	})
